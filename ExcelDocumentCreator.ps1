48d97610f653b6e6f632e1ebb03eff7dbcd485d5
Stefan Schnell <StefanSchnell@users.noreply.github.com>
2015-09-24 14:49:21 +0200

    Rename ExcelDocumentCreator.ps1 #-Begin----------------------------------------------------------------- #- #- Excel document creator #- #- Creates different types of table documents automatically with #- different count of sheets #- #- Author: Stefan Schnell #- #-----------------------------------------------------------------------    #-Constants-----------------------------------------------------------     $xlExcel9795 = 43     $xlWorkbookDefault = 51     $xlOpenDocumentSpreadsheet = 60    #-Sub Main------------------------------------------------------------     Function Main () {        #-Configure count of sheets---------------------------------------         $cntSheets = 100        #-Configure output format-----------------------------------------         $Format = $xlWorkbookDefault         $oExcel = New-Object -ComObject "Excel.Application"       $oExcel.Visible = $True        $oWorkBook = $oExcel.Workbooks.Add()       For ($i = 1; $i -le $cntSheets; $i++) {         Try {           $oWorkBook.Sheets.Item($i).Select()         }         Catch {           $oWorkBook.Sheets.Add([System.Reflection.Missing]::Value,              $oWorkBook.Sheets.Item($oWorkBook.Sheets.Count)) > $Null         }          For ($j = 1; $j -le 10; $j++) {           $oWorkBook.Sheets.Item($i).Cells.Item($j, 1).Value2 =              "Dies ist ein Test " + $j         }        }        $GUID = [GUID]::NewGuid()       $FileName = $env:USERPROFILE + "\Documents\" + $GUID       $oWorkBook.SaveAs($FileName, $Format)        $oExcel.Quit()      }    #-Main----------------------------------------------------------------     If ($PSVersionTable.PSVersion.Major -ge 3) {       Main     }  #-End------------------------------------------------------------------- to ExcelDocumentCreator.ps1

---
+++ ExcelDocumentCreator.ps1
@@ -0,0 +1,61 @@
+
+#-Begin-----------------------------------------------------------------
+#-
+#- Excel document creator
+#-
+#- Creates different types of table documents automatically with
+#- different count of sheets
+#-
+#- Author: Stefan Schnell
+#-
+#-----------------------------------------------------------------------
+
+  #-Constants-----------------------------------------------------------
+    $xlExcel9795 = 43
+    $xlWorkbookDefault = 51
+    $xlOpenDocumentSpreadsheet = 60
+
+  #-Sub Main------------------------------------------------------------
+    Function Main () {
+
+      #-Configure count of sheets---------------------------------------
+        $cntSheets = 100
+
+      #-Configure output format-----------------------------------------
+        $Format = $xlWorkbookDefault
+
+
+      $oExcel = New-Object -ComObject "Excel.Application"
+      $oExcel.Visible = $True
+
+      $oWorkBook = $oExcel.Workbooks.Add()
+      For ($i = 1; $i -le $cntSheets; $i++) {
+        Try {
+          $oWorkBook.Sheets.Item($i).Select()
+        }
+        Catch {
+          $oWorkBook.Sheets.Add([System.Reflection.Missing]::Value, 
+            $oWorkBook.Sheets.Item($oWorkBook.Sheets.Count)) > $Null
+        }
+
+        For ($j = 1; $j -le 10; $j++) {
+          $oWorkBook.Sheets.Item($i).Cells.Item($j, 1).Value2 = 
+            "Dies ist ein Test " + $j
+        }
+
+      }
+
+      $GUID = [GUID]::NewGuid()
+      $FileName = $env:USERPROFILE + "\Documents\" + $GUID
+      $oWorkBook.SaveAs($FileName, $Format)
+
+      $oExcel.Quit()
+
+    }
+
+  #-Main----------------------------------------------------------------
+    If ($PSVersionTable.PSVersion.Major -ge 3) {
+      Main
+    }
+
+#-End-------------------------------------------------------------------
